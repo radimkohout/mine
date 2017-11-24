0be0612fb1400922e594dc87c5b2bac31d27130f
root <root@rsys>
2017-11-24 16:43:43 +0000

    test

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake screen
-screen
 git clone https://github.com/JayDDee/cpuminer-opt.git
 cd cpuminer-opt
 ./build.sh
-./cpuminer -a cryptonight -o -u stratum+tcp://bcn.pool.minergate.com:45550 radimm.kohout@gmail.com -p x
+screen
