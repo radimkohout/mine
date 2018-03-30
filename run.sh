c0a2701ed576c5776a0c5e7d7bc01c9e9ba73e22
radimkohout <radimm.kohout@gmail.com>
2018-03-30 09:37:18 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake screen
+sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake screen zlib1g-dev
 git clone https://github.com/JayDDee/cpuminer-opt.git
 cd cpuminer-opt
 ./build.sh
