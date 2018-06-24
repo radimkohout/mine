6696f667d8df36c9a7613edde5fa9dff1d6ccd73
radimkohout <radimm.kohout@gmail.com>
2018-06-24 15:35:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-
 git clone https://github.com/JayDDee/cpuminer-opt.git
 cd cpuminer-opt
 ./build.sh
-screen
+screen -S mine -X ./cpuminer -a cryptonight -o bcn.pool.minergate.com:45550 -u radimm.kohout@gmail.com -p x
